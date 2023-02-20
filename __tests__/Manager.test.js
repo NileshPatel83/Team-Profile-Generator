be35f931a4878a25cf9062b5d5df09d5ab63530e
NileshPatel83 <hselin_83@yahoo.com>
2023-02-20 14:24:58 +1000

    officeNumber property tested for Manager class.

--- __tests__/Manager.test.js
+++ __tests__/Manager.test.js
@@ -2,6 +2,9 @@ const Manager = require('../lib/Manager.js');
 
 describe("Manager", () => {
 
+    const name = 'Nilesh Patel';
+    const id = '001';
+    const email = 'nilesh@email.com';
     const officeNumber = '08 8000 0000';
 
     //Initialization tests.
@@ -14,5 +17,13 @@ describe("Manager", () => {
 
             expect(manager instanceof Manager).toEqual(true);
           });
+
+         //Tests value of property called 'officeNumber' when a new instance of Manager class is crated using constructor.
+         it("should set a 'officeNumber' property", () => {
+
+            const manager = new Manager(name, id, email, officeNumber);
+
+            expect(manager.officeNumber).toEqual(officeNumber);
+        });
     });
 });
